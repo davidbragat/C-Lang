a1793d6532cd4e6c7d7ac6f8584551dd624a420f
davidbragat <davidbragat@gmail.com>
2020-07-03 01:49:17 -0300

    Update Grafos.c
    Implement Grafos

--- Grafos.c
+++ Grafos.c
@@ -4,18 +4,64 @@
 
 #define maximo 10
 
+//Variables
 int tam = 0;
 int grafo[maximo];
 int ma[maximo][maximo];
+int option;
 
+//Prototypes
+void menu();
 int grafo_tamanho();
 void inserir_aresta();
+void remover_aresta();
+void print_vetor();
+void print_ma();
 
 void main(){
 setlocale(LC_ALL, "Portuguese");
 
-    grafo_tamanho();
-    inserir_aresta();
+    do{
+        menu();
+        scanf("%d", &option);
+        switch (option){
+        case 1:
+            grafo_tamanho();
+            break;
+
+        case 2:
+            inserir_aresta();
+            break;
+        
+        case 3: 
+            remover_aresta();
+            break;
+        
+        case 4: 
+            print_vetor();
+            break;
+        
+        case 5: 
+            print_ma();
+            break;
+
+        default:
+            break;
+        }
+    } while (option != 0);
+
+}//END MAIN FUNCTION
+
+void menu(){
+    system("cls");
+    printf("Selecione uma das opções\n");
+    printf("1. Criar Grafo\n");
+    printf("2. Inserir Aresta\n");
+    printf("3. Remover Aresta\n");
+    printf("4. Imprimir Vetor\n");
+    printf("5. Imprimir Matriz\n");
+
+    printf("0. Sair\n");
 
 }
 
@@ -36,8 +82,9 @@ void inserir_aresta(){
 
     if(div < 0 || div > tam-1 || conv < 0 || conv > tam - 1){
         printf("Valor inserido está fora do intervalo permitido\n");
-        printf("O valor deve estar entre 0 e %d", tam-1);
+        printf("O valor deve estar entre 0 e %d\n", tam-1);
         system("pause");
+        inserir_aresta();
     }
     else{
         ma[div][conv] = 1;
@@ -55,8 +102,9 @@ void remover_aresta(){
 
     if(div < 0 || div > tam-1 || conv < 0 || conv > tam - 1){
         printf("Valor inserido está fora do intervalo permitido\n");
-        printf("O valor deve estar entre 0 e %d", tam-1);
+        printf("O valor deve estar entre 0 e %d\n", tam-1);
         system("pause");
+        remover_aresta();
     }
     else{
         ma[div][conv] = 0;
@@ -68,16 +116,17 @@ void print_vetor(){
     for (int i = 0; i < tam; i++){
         printf("%d, ", grafo[i]);
     }
+    system("\npause");
 }
 
 void print_ma(){
-    printf("Matriz de adjacências\n[");
+    printf("Matriz de adjacências\n");
     for (int i = 0; i < tam; i++){
         for (int j = 0; j < tam; j++){
-            printf("%d", ma[i][j]);
+            printf("%d ", ma[i][j]);
         }
         printf("\n");
     }
-    printf("]");
     
+    system("\npause");
 }
